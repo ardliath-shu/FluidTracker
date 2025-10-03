68c1a368db5e18553cf7fb9885fe513852a09186
covenesme <84377742+CovenEsme@users.noreply.github.com>
2025-10-03 14:34:52 +0100

    Add better styling for the bottle and water level

--- src/app/components/Bottle.jsx
+++ src/app/components/Bottle.jsx
@@ -2,20 +2,28 @@ import { useState } from "react";
 import "../../../public/css/bottle.css";
 import BottleWaterLevel from "./BottleWaterLevel";
 
+import Image from "next/image";
+import bottleImage from "../../../public/images/bottle.svg";
+
 const Bottle = ({ target, currentFluid, changeFluidAmount }) => {
   return (
     <div>
       <div className="bottlecontainer">
         <div id="bottle">
           <BottleWaterLevel levelPercent={(currentFluid / target) * 100} />
+          <Image
+            id="bottleimage"
+            src={bottleImage}
+            alt="A water bottle."
+          />
         </div>
 
         {/* Proof of concept water level controls */}
         {/* The final controls will behave a lot differently to this ^^' */}
-        <button className="fluidbutton" onClick={() => changeFluidAmount(+50)}>
+        <button className="fluidbutton" onClick={() => changeFluidAmount(+100)}>
           Add 50ml
         </button>
-        <button className="fluidbutton" onClick={() => changeFluidAmount(-50)}>
+        <button className="fluidbutton" onClick={() => changeFluidAmount(-100)}>
           Remove 50ml
         </button>
       </div>
