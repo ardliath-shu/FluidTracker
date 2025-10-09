23e57b2d75ad4f57c4e8136258f4acaa056a41db
ardliath-shu <c5045540@hallam.shu.ac.uk>
2025-10-09 16:12:31 +0100

    Created calls from DB

---
+++ src/app/lib/connection.js
@@ -0,0 +1,8 @@
+import mysql from "mysql2/promise";
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_SCHEMA,
+});
+export default connection;
