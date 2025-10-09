da743c55994a1bc31a84a4e5a6c2be8bf1862759
jazasjam <jonathan.zasada-james@student.shu.ac.uk>
2025-10-09 22:06:34 +0100

    Ran prettier

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,15 +1,13 @@
 import mysql from "mysql2/promise";
 import connection from "./connection";
 
-
-
 const fetchUser = async (user_id) => {
   try {
-    const query = "select * from users where userId = ?"; 
-    const [rows] = await connection.execute(query, [user_id]); 
+    const query = "select * from users where userId = ?";
+    const [rows] = await connection.execute(query, [user_id]);
     return rows;
   } catch (error) {
-    console.error("Database Error:", error); 
+    console.error("Database Error:", error);
     throw new Error("Failed to fetch data.");
   }
 };
--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,15 +1,14 @@
 import { fetchUser } from "../lib/db";
 
 const TestPage = async () => {
-  
   const result = await fetchUser(1);
-    const user = result[0];
-console.log(user);
+  const user = result[0];
+  console.log(user);
   return (
     <main>
       <div>
-        <h1>Hello {user.email}</h1>        
-      </div>      
+        <h1>Hello {user.email}</h1>
+      </div>
     </main>
   );
 };
