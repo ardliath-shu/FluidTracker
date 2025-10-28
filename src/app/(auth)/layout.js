e99f02614d69a3b6e48f6d32eaf8d5ea1c5d20c8
covenesme <84377742+CovenEsme@users.noreply.github.com>
2025-10-28 15:01:19 +0000

    Add DarkModeToggle to auth pages

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,10 +1,11 @@
 import { siteConfig } from "@/app/lib/site.config";
+import DarkModeToggle from "../components/DarkModeToggle";
 
 export default function AuthLayout({ children }) {
   return (
     <div className="auth-layout">
       <h1>
-        <i className="fa fa-droplet"></i> {siteConfig.name}
+        <i className="fa fa-droplet"></i> {siteConfig.name} <DarkModeToggle />
       </h1>
       <main>{children}</main>
     </div>
