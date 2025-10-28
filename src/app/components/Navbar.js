c569a1f9c5e9069c6af1a1509d3d59f88f1ecb76
covenesme <84377742+CovenEsme@users.noreply.github.com>
2025-10-28 15:02:48 +0000

    Add h1 to pages using the Navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,15 +1,16 @@
 "use client";
+
 import { siteConfig } from "@/app/lib/site.config";
 import Link from "next/link";
 
 export default function Navbar({ onToggleSidebar }) {
   return (
     <nav className="navbar">
-      <div className="brand">
+      <h1 className="brand">
         <Link href="/">
           <i className="fa fa-droplet"></i> {siteConfig.name}
         </Link>
-      </div>
+      </h1>
 
       <button className="burger" onClick={onToggleSidebar}>
         &#9776; {/* burger menu icon */}
